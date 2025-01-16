6655f426c4557dd7ccf1ed43e555034c5a97c464
damiruimo <damirusanhitha05@gmail.com>
2025-01-16 08:04:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,13 +2,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
-	SESSION_ID: process.env.SESSION_ID || '',
-	SUDO: process.env.SUDO || '',
+	SESSION_ID: process.env.SESSION_ID || 'XSTRO_99_78_17',
+	SUDO: process.env.SUDO || '94768806865',
 	API_ID: process.env.API_ID || 'https://xstro-api-4fb28ece11a9.herokuapp.com',
 	BOT_INFO: process.env.BOT_INFO || 'αѕтяσχ11;χѕтяσ м∂',
 	STICKER_PACK: process.env.STICKER_PACK || 'мα∂є ву;χѕтяσ мυℓтι ∂єνι¢є вσт',
 	WARN_COUNT: process.env.WARN_COUNT || 3,
-	TIME_ZONE: process.env.TIME_ZONE || 'Africa/Lagos',
+	TIME_ZONE: process.env.TIME_ZONE || 'Asia/Colombo',
 	DEBUG: process.env.DEBUG || false,
 	VERSION: '1.2.3'
 };
